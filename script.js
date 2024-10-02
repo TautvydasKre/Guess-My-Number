5d26169d6e24dd43b02fe329b106dbfb0525da42
TautvydasKre <144778291+TautvydasKre@users.noreply.github.com>
2024-10-02 16:07:15 +0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log(document.querySelector('.message'));
+const doc = document;
+
+console.log(doc.querySelector('.message').textContent);
+
+doc.querySelector('.message').textContent = '🎉 Correct Number!';
+
+doc.querySelector('.number').textContent = 13;
+doc.querySelector('.score').textContent = 10;
+
+doc.querySelector('.guess').value = 23;
+console.log(doc.querySelector('.guess').value);
