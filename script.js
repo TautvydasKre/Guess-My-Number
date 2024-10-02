a676262c5da139183e212acd5c3df2f0ced0f0c6
TautvydasKre <144778291+TautvydasKre@users.noreply.github.com>
2024-10-02 16:38:40 +0300

    update

--- script.js
+++ script.js
@@ -2,12 +2,25 @@
 
 const doc = document;
 
-console.log(doc.querySelector('.message').textContent);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+doc.querySelector('.number').textContent = secretNumber;
 
-doc.querySelector('.message').textContent = '🎉 Correct Number!';
+doc.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(doc.querySelector('.guess').value);
+  console.log(guess, typeof guess);
 
-doc.querySelector('.number').textContent = 13;
-doc.querySelector('.score').textContent = 10;
-
-doc.querySelector('.guess').value = 23;
-console.log(doc.querySelector('.guess').value);
+  if (!guess) {
+    doc.querySelector('.message').textContent = '⛔️ No number!';
+  } else if (guess === secretNumber) {
+    doc.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    doc.querySelector('.message').textContent = '📈Too high! ';
+    score--;
+    doc.querySelector('.score').textContent = score;
+  } else if (guess < secretNumber) {
+    doc.querySelector('.message').textContent = '📉Too low!';
+    score--;
+    doc.querySelector('.score').textContent = score;
+  }
+});
